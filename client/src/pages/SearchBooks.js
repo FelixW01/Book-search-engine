3cca3544fb39f6b164339bfeb0b59cf0d6b579e9
Felix W <felixwillem01@yahoo.com>
2023-08-15 20:43:35 -0400

    refactored savedBooks.js to use loading, useMutation and useQuery

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -9,11 +9,10 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/react-hooks'
 import { SAVE_BOOK } from '../utils/mutations';
-import { GET_ME } from '../utils/queries';
 
 
 const SearchBooks = () => {
